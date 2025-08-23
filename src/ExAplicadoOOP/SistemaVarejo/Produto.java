537a2bf8316e9763d7c1ba660f1ce2e6def50fea
JuanHGomes <juanhgomes@gmail.com>
2025-08-23 11:28:34 -0300

    Correçâo classe main

--- src/ExAplicadoOOP/SistemaVarejo/Produto.java
+++ src/ExAplicadoOOP/SistemaVarejo/Produto.java
@@ -4,9 +4,21 @@ public abstract class Produto implements IOperacoesProduto {
     private String codigo;
     private String nome;
     private double preco;
-    private String clienteComprador;
+    private Cliente comprador;
 
+    public Produto(String codigo, String nome, double preco, Cliente comprador) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.comprador = comprador;
+    }
 
+    public abstract void calcularFrete();
 
+    public void aplicarDesconto(){
 
+    }
+    public void aplicarAumento(){
+
+    }
 }
