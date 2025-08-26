792516060761be52517066431c8b7ec0dc48778b
Juan Gomes <juanhgomes@gmail.com>
2025-08-26 11:52:14 -0300

    Excecoes - inicio

--- src/MiniHackathon25_08/PolimorfismoAbstracao/Main.java
+++ src/MiniHackathon25_08/PolimorfismoAbstracao/Main.java
@@ -2,11 +2,8 @@ package MiniHackathon25_08.PolimorfismoAbstracao;
 
 public class Main {
     public static void main(String[] args) {
-
-
         Investimento rendaFixa = new RendaFixa(1000);
         Investimento rendaVariavel = new RendaVariavel(1000);
-
 
         Investimento[] investVetor = new Investimento[]{rendaFixa,rendaVariavel};
 
